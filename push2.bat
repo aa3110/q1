b5ec1e45e94ae6b23c3171bf561be6a321b54a39
aa3110 <m.cesena@web.de>
2023-07-04 22:55:39 +0200

    added_ 230704

--- push2.bat
+++ push2.bat
@@ -4,10 +4,5 @@ git checkout -b "%a1%"
 git add .
 git commit -m "added_ "%a1%""
 git remote add origin https://github.com/aa3110/q2.git
-git config --global user.name "aa3110"
-git config user.name "aa3110"
-git config --global user.email "m.cesena@web.de"
-git config user.email "m.cesena@web.de"
-git push --set-upstream origin "%a1%"
 git push --force origin
 pause
